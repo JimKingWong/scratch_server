c3e9c21d961059d42db01a96baef91d13ee03a9a
wong <wangjijian1994@gmail.com>
2025-08-02 14:48:18 -0300

    1

--- application/common/service/Game.php
+++ application/common/service/Game.php
@@ -7,7 +7,10 @@ use app\common\model\MoneyLog;
 use app\common\model\Order;
 use app\common\model\User;
 use fast\Random;
+use think\Cache;
 use think\Db;
+use think\Log;
+use think\view\driver\Think;
 
 /**
  * 游戏服务
@@ -34,6 +37,11 @@ class Game extends Base
             $this->error(__('游戏不存在'));
         }
 
+        $check_order = db('order')->where('cate_id', $cate_id)->where('user_id', $this->auth->id)->where('status', 1)->find();
+        if ($check_order) {
+            $this->error(__('您本次已购买过该游戏'));
+        }
+
         $user = $this->auth->getUser();
         
         if($user->money < $cate->price){
@@ -42,60 +50,121 @@ class Game extends Base
 
         $order_no = date('YmdHis') . mt_rand(100000, 999999);
 
-        $result = false;
-        Db::startTrans();
-        try{
-            $user = User::lock(true)->find($user->id);
-            $before = $user->money;
-            $after = $user->money - $cate->price;
-            $user->money = $after;
+        $redis = Cache::store('redis')->handler();
+        // 并发处理
+        $lock_key = 'buy:lock_' . $this->auth->id;
 
-            $result = $user->save();
+        $is_lock = $redis->setnx($lock_key, 0.5); // 加锁
 
-            if(MoneyLog::create([
-                'admin_id'          => $user->admin_id,
-                'user_id'           => $user->id,
-                'type'              => 'buy_goods',
-                'before'            => $before,
-                'after'             => $after,
-                'money'             => $cate->price,
-                'memo'              => '购买游戏',
-                'transaction_id'    => $order_no,
-            ]) === false){
-                $result = false;
+        if(!$is_lock){
+            // 释放锁
+            $redis->del($lock_key);
+            $this->error(__('请勿重复提交'));
+        }
+
+        $winItem = db('goods_cate')->where('cate_id', $cate_id)->field('id,name,price,odds,is_win,image')->select();
+
+        $arr = [];
+        $goods = [];
+        foreach($winItem as $v){
+            $arr[$v['id']] = $v['odds'] * 10000;
+            $goods[$v['id']] = $v;
+        }
+        
+        // 抽中的格子
+        $goods_id = Random::lottery($arr);
+        $goods = $goods[$goods_id];
+        // dd($goods);
+
+        // 获取九宫格
+        $grid = $this->getGoodsGrid($cate_id, $goods);
+        
+        if($is_lock){
+            $result = false;
+            Db::startTrans();
+            try{
+                $user = User::lock(true)->find($user->id);
+
+                // 避免并发超出
+                if($user->money < $cate->price){
+                    Db::rollback();
+                    $this->error(__('余额不足'));
+                }
+                $before = $user->money;
+                $after = $user->money - $cate->price;
+                $user->money = $after;
+
+                $result = $user->save();
+
+                if(MoneyLog::create([
+                    'admin_id'          => $user->admin_id,
+                    'user_id'           => $user->id,
+                    'type'              => 'buy_goods',
+                    'before'            => $before,
+                    'after'             => $after,
+                    'money'             => $cate->price,
+                    'memo'              => '购买游戏',
+                    'transaction_id'    => $order_no,
+                ]) === false){
+                    $result = false;
+                }
+
+                // 创建订单并生成对应订单的九宫格
+                $data = [
+                    'admin_id'  => $user->admin_id,
+                    'user_id'   => $user->id,
+                    'cate_id'   => $cate_id,
+                    'order_no'  => $order_no,
+                    'status'    => 1,
+                    'num'       => 1,
+                    'is_win'    => $goods['is_win'],
+                    'goods_cate_id' => $goods_id,
+                    'grid'      => json_encode($grid),
+                    'price'     => $cate->price,
+                    'paytime'   => datetime(time()),
+                ];
+                
+                if(Order::create($data) === false){
+                    $result = false;
+                }
+                
+                if($result != false){
+                    Db::commit();
+                }
+                
+            }catch(\Exception $e) {
+                Log::record($e->getMessage());
+                // 释放锁
+                $redis->del($lock_key);
+                Db::rollback();
+                $this->error(__('购买失败'));
             }
 
-            $data = [
-                'admin_id'  => $user->admin_id,
-                'user_id'   => $user->id,
-                'cate_id'   => $cate_id,
-                'order_no'  => $order_no,
-                'status'    => 1,
-                'num'       => 1,
-                'price'     => $cate->price,
-                'paytime'   => datetime(time()),
-            ];
-
-            if(Order::create($data) === false){
-                $result = false;
+            // 释放锁
+            $redis->del($lock_key);
+        }else{
+            // 防止死锁
+            if($redis->ttl($lock_key) == -1){
+                $redis->expire($lock_key, 1);
             }
-
-            if($result != false){
-                Db::commit();
-            }
-            
-        }catch(\Exception $e) {
-            Db::rollback();
-            $this->error(__('购买失败'));
         }
         
         if($result === false){
             $this->error(__('购买失败'));
         }
 
-        $this->success(__('请求成功'), ['order_no' => $order_no, 'money' => number_format($user->money, 2)]);
+        $retval = [
+            'order_no' => $order_no,
+            'money'    => number_format($user->money, 2),
+            'grid'     => $grid,
+        ];
+
+        $this->success(__('请求成功'), $retval);
     }
     
+    /**
+     * 检查是否可以玩
+     */
     public function checkPlay($user, $cate_id)
     {
         $where['user_id'] = $user->id;
@@ -108,44 +177,7 @@ class Game extends Base
         
         return $order;
     }
-    
-    /**
-     * 生成游戏
-     */
-    public function create()
-    {
-        $cate_id = $this->request->post('cate_id');
-
-        $user = $this->auth->getUser();
-        
-        $this->checkPlay($user, $cate_id);
-
-        $where['user_id'] = $user->id;
-        $where['cate_id'] = $cate_id;
-        $where['status'] = 0;
-        $game = db('game_record')->where($where)->field('id,roundid')->find();
-
-        if($game){
-            $this->success(__('请求成功'), ['roundid' => $game['roundid'], 'money' => number_format($user->money, 2)]);
-        }
-
-        $roundid = $user->id . '_' . $cate_id . '_' . date('YmdHis');
-
-        // 创建游戏
-        $data = [
-            'admin_id'  => $user->admin_id,
-            'user_id'   => $user->id,
-            'cate_id'   => $cate_id,
-            'roundid'   => $roundid,
-            'status'    => 0,
-            'createtime'=> datetime(time()),
-            'updatetime'=> datetime(time()),
-        ];
-
-        db('game_record')->insert($data);
-        $this->success(__('请求成功'), ['roundid' => $roundid, 'money' => number_format($user->money, 2)]);
-    }
-
+   
     /**
      * 开始游戏
      */
@@ -155,118 +187,115 @@ class Game extends Base
 
         $user = $this->auth->getUser();
 
+        // 当前订单数据
         $order = $this->checkPlay($user, $cate_id);
+    
+        $grid = json_decode($order['grid'], true);
         
-        // $where['user_id'] = $user->id;
-        // $where['cate_id'] = $cate_id;
-        // $where['status'] = '0';
-        // $game = db('game_record')->where($where)->field('id,roundid')->find();
-        // // dd($game);
-        // if (!$game) {
-        //     $this->error(__('游戏不存在'));
-        // }
+        $goods_id = $order['goods_cate_id'];
 
-        $winItem = db('goods_cate')->where('cate_id', 1)->field('id,name,price,odds,is_win,image')->select();
+        $is_win = $order['is_win'];
 
-        $arr = [];
-        $goods = [];
-        foreach($winItem as $v){
-            $arr[$v['id']] = $v['odds'] * 10000;
-            $goods[$v['id']] = $v;
-        }
-        
-        // 抽中的格子
-        $goods_id = Random::lottery($arr);
-        // dd($goods);
-        $goods = $goods[$goods_id];
-        // $goods = $goods[1];
+        $goods = db('goods_cate')->where('id', $goods_id)->field('id,name,price,image')->find();
+        $goods['price'] = number_format($goods['price'], 2);
         $goods['image'] = $goods['image'] ? cdnurl($goods['image']) : '';
-
-        // 获取九宫格
-        $grid = $this->getGoodsGrid(1, $goods);
-
+        
         $roundid = $user->id . '_' . $cate_id . '_' . date('YmdHis');
 
-        $result = false;
-        Db::startTrans();
-        try{
-            $user = User::lock(true)->find($user->id);
-            
-            $data = [
-                'admin_id'  => $user->admin_id,
-                'user_id'   => $user->id,
-                'cate_id'   => $cate_id,
-                'roundid'   => $roundid,
-                'status'    => 1,
-                'goods_cate_id' => $goods_id,
-                'is_win'        => $goods['is_win'],
-                'prizes'    => json_encode($goods),
-                'win_amount'=> $goods['price'],
-                'createtime'=> datetime(time()),
-                'updatetime'=> datetime(time()),
-                'endtime'   => datetime(time()),
-            ];
-            $result = db('game_record')->insert($data);
-            // $result = db('game_record')->where('id', $game['id'])->update([
-            //     'status'        => 1, 
-            //     'goods_cate_id' => $goods_id, 
-            //     'win_amount'    => $goods['price'],
-            //     'is_win'        => $goods['is_win'],
-            //     'prizes'        => json_encode($goods),
-            //     'updatetime'    => datetime(time()),
-            //     'endtime'       => datetime(time()),
-            // ]);
+        $redis = Cache::store('redis')->handler();
+        // 并发处理
+        $lock_key = 'play:lock_' . $this->auth->id;
 
-            if(db('order')->where('id', $order['id'])->update(['status' => 2]) === false){
-                $result = false;
-            }
-        
-            if($goods['is_win'] > 0){
-                $before = $user->money;
-                $after = $user->money + $goods['price'];
-                $user->money = $after;
-                $user->bonus = $user->bonus + $goods['price'];
-
-                if($user->save() === false){
-                    $result = false;
-                }
-
-                $user->userdata->total_profit = $user->userdata->total_profit + $goods['price'];
-                $user->userdata->today_profit = $user->userdata->today_profit + $goods['price'];
-                if($user->userdata->save() === false){
-                    $result = false;
-                }
-
-                if(MoneyLog::create([
-                    'admin_id'          => $user->admin_id,
-                    'user_id'           => $user->id,
-                    'type'              => 'lottery',
-                    'before'            => $before,
-                    'after'             => $after,
-                    'money'             => $goods['price'],
-                    'memo'              => '刮刮乐中奖',
-                    'transaction_id'    => $roundid,
-                ]) === false){
-                    $result = false;
-                }
-            }
-            
-            if($result != false){
-                Db::commit();
-            }
-
-        }catch(\Exception $e){
-            echo $e->getMessage();
-            Db::rollback();
-            $this->error(__('请求失败'));
+        $is_lock = $redis->setnx($lock_key, 1); // 加锁
+        // dd($is_lock);
+        if(!$is_lock){
+            // 释放锁
+            $redis->del($lock_key);
+            $this->error(__('请勿重复提交'));
         }
 
+        if($is_lock){
+            $result = false;
+            Db::startTrans();
+            try{
+                $user = User::lock(true)->find($user->id);
+                
+                $data = [
+                    'admin_id'  => $user->admin_id,
+                    'user_id'   => $user->id,
+                    'cate_id'   => $cate_id,
+                    'roundid'   => $roundid,
+                    'status'    => 1,
+                    'goods_cate_id' => $goods_id,
+                    'is_win'        => $is_win,
+                    'prizes'    => json_encode($goods),
+                    'win_amount'=> $goods['price'],
+                    'createtime'=> datetime(time()),
+                    'updatetime'=> datetime(time()),
+                    'endtime'   => datetime(time()),
+                ];
+                $result = db('game_record')->insert($data);
+
+                if(db('order')->where('id', $order['id'])->update(['status' => 2]) === false){
+                    $result = false;
+                }
+            
+                if($is_win > 0){
+                    $before = $user->money;
+                    $after = $user->money + $goods['price'];
+                    $user->money = $after;
+                    $user->bonus = $user->bonus + $goods['price'];
+
+                    if($user->save() === false){
+                        $result = false;
+                    }
+
+                    $user->userdata->total_profit = $user->userdata->total_profit + $goods['price'];
+                    $user->userdata->today_profit = $user->userdata->today_profit + $goods['price'];
+                    if($user->userdata->save() === false){
+                        $result = false;
+                    }
+
+                    if(MoneyLog::create([
+                        'admin_id'          => $user->admin_id,
+                        'user_id'           => $user->id,
+                        'type'              => 'lottery',
+                        'before'            => $before,
+                        'after'             => $after,
+                        'money'             => $goods['price'],
+                        'memo'              => '刮刮乐中奖',
+                        'transaction_id'    => $roundid,
+                    ]) === false){
+                        $result = false;
+                    }
+                }
+                
+                if($result != false){
+                    Db::commit();
+                }
+
+            }catch(\Exception $e){
+                $redis->del($lock_key);
+                Log::record($e->getMessage());
+                Db::rollback();
+                $this->error(__('请求失败'));
+            }
+
+            // 释放锁
+            $redis->del($lock_key);
+        }else{
+            // 防止死锁
+            if($redis->ttl($lock_key) == -1){
+                $redis->expire($lock_key, 5);
+            }
+        }
+        
         $win_amount = db('game_record')->where('cate_id', $cate_id)->where('user_id', $user->id)->sum('win_amount');
         $retval = [
+            'money'      => number_format($user->money, 2),
             'win_amount' => $win_amount,
             'award_item' => $goods,
             'item'       => $grid,
-            'money'      => number_format($user->money, 2),
         ];
 
         $this->success(__('请求成功'), $retval);
