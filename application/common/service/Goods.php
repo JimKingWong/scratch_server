ca538652025d5a036edb9f0c11b97c297bbf6870
wong <wangjijian1994@gmail.com>
2025-08-02 07:10:54 -0300

    1

--- application/common/service/Goods.php
+++ application/common/service/Goods.php
@@ -4,9 +4,6 @@ namespace app\common\service;
 
 use app\common\model\Cate;
 use app\common\model\GoodsCate;
-use app\common\model\Grid;
-use app\common\model\Order;
-use app\common\service\util\Scratch;
 
 /**
  * 商品
@@ -34,18 +31,17 @@ class Goods extends Base
 
         $cate = Cate::where('id', $cate_id)->field('id,name,title,intro,price,image')->find();
 
-        $fields = "id,cate_id,goods_id";
+        $fields = "id,cate_id,goods_id,image";
         $list = GoodsCate::where($where)->field($fields)->order('weigh desc')->select();
         // dd($list);
-        $goodsList = [];
+        
         foreach($list as $val){
-            $val->goods->image = $val->goods->image ? cdnurl($val->goods->image) : '';
-            $goodsList[] = $val->goods;
+            $val->image = $val->image ? cdnurl($val->image) : '';
         }
         
         $retval = [
             'cate' => $cate,
-            'list' => $goodsList,
+            'list' => $list,
         ];
         $this->success(__('请求成功'), $retval);
     }
