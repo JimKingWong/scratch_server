bc10566d8fc923858635867bcd00d119f265f2ec
wong <wangjijian1994@gmail.com>
2025-08-03 14:53:50 -0300

    1

--- application/common/library/Auth.php
+++ application/common/library/Auth.php
@@ -26,7 +26,7 @@ class Auth
     //默认配置
     protected $config = [];
     protected $options = [];
-    protected $allowFields = ['id', 'username', 'nickname', 'money', 'email', 'bonus'];
+    protected $allowFields = ['id', 'username', 'nickname', 'money', 'email', 'bonus', 'name'];
 
     public function __construct($options = [])
     {
